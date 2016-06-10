2ca1b379d72b371aa425887fa996ca9ee9ebf07b
Patrick <patrick.rodgers@microsoft.com>
2016-06-10 14:57:23 -0400

    updated test

--- test/app.js
+++ test/app.js
@@ -6,13 +6,20 @@ var helpers = require('yeoman-test');
 describe('example:app', function () {
   before(function () {
     return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({someAnswer: true})
       .toPromise();
   });
 
   it('creates files', function () {
     assert.file([
-      'dummyfile.txt'
+      "gulpfile.js",
+      "tsconfig.json",
+      "tslint.json",
+      "typings.json",
+      "webpack.config.js",
+      "src/app.ts",
+      "src/helloworld.ts",
+      "src/sp.ts",
+      "src/sp2.ts"
     ]);
   });
 });
